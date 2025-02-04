6a7534e9ff56d5244d49062db68edf4af67f5099
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-04 17:35:24 -0300

    css completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DefaultLoginLayoutComponent } from './default-login-layout/default-login-layout/default-login-layout.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DefaultLoginLayoutComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'login';
+  title = '';
 }
