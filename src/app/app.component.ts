75e9fc264678e8641ac2f2eceddbaa9f9f11979c
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-04 18:44:53 -0300

    front completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DefaultLoginLayoutComponent } from './default-login-layout/default-login-layout/default-login-layout.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PrimaryInputComponent } from './components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DefaultLoginLayoutComponent, LoginComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    DefaultLoginLayoutComponent, 
+    LoginComponent, 
+    PrimaryInputComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
